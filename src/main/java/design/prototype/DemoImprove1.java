a7c340bb384c19c8e2d1d68304cb06c2dbd8b391
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2021-07-12 10:17:54 +0800

    原型模式 map的深浅克隆使用示例

---
+++ src/main/java/design/prototype/DemoImprove1.java
@@ -0,0 +1,37 @@
+package design.prototype;
+
+import design.prototype.entity.SearchWord;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * 任何时刻，系统A中的所有数据都必须是同一个版本的，要么都是版本a，要么都是版本b，不能有的是版本 a，有的是版本 b
+ * 还要求：在更新内存数据的时候，系统 A 不能处于不可用状态，也就是不能停机更新数据
+ *
+ * @Author 喻可
+ * @Date 2021/7/9 18:10
+ */
+public class DemoImprove1 {
+
+    private HashMap<String, SearchWord> currentKeywords = new HashMap<>();
+
+    public void refresh() {
+        HashMap<String, SearchWord> newKeywords = new LinkedHashMap<>();
+
+        // 从数据库中取出所有的数据，放入到newKeywords中
+        List<SearchWord> toBeUpdatedSearchWords = getSearchWords();
+        for (SearchWord searchWord : toBeUpdatedSearchWords) {
+            newKeywords.put(searchWord.getKeyWord(), searchWord);
+        }
+
+        currentKeywords = newKeywords;
+    }
+
+    private List<SearchWord> getSearchWords() {
+        // TODO: 从数据库中取出所有的数据
+        return Collections.emptyList();
+    }
+}
---
+++ src/main/java/design/prototype/entity/SearchWord.java
@@ -0,0 +1,19 @@
+package design.prototype.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Author 喻可
+ * @Date 2021/7/9 17:59
+ */
+@Data
+@AllArgsConstructor
+public class SearchWord {
+
+    private long lastUpdateTime;
+
+    private String keyWord;
+
+    private long count;
+}
