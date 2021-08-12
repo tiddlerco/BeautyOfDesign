befaf8f74fddcbc6235f96b6e9d19542cd41a213
wb-yk938100 <wb-yk938100@alibaba-inc.com>
2021-08-12 11:28:53 +0800

    java8function作为参数使用

---
+++ src/main/java/functionlearn/FunctionClass.java
@@ -0,0 +1,51 @@
+package functionlearn;
+
+import java.util.function.Function;
+
+/**
+ * @Author 喻可
+ * @Date 2021/8/12 11:02
+ */
+public class FunctionClass<T> {
+
+
+    public String useFunction(String str, Function<String, String> method) {
+
+        String result;
+
+        result = str + method.apply("ykk");
+
+        return result;
+    }
+
+    public String useFunction2(String str, Function<Integer, Integer> method) {
+
+        String result;
+
+        result = str + method.apply(20);
+
+        return result;
+    }
+
+    public static <T> T getStr(T str) {
+
+        return  str;
+    }
+
+
+    public static void main(String[] args) {
+
+        FunctionClass functionClass = new FunctionClass();
+
+        String result = functionClass.useFunction("my name is ", f -> getStr(f));
+
+        String result2 = functionClass.useFunction2("my age is ", f -> getStr(f));
+
+
+        System.out.println(result);
+
+        System.out.println(result2);
+
+    }
+
+}
